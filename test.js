6b626f12391bb3b57859dcd6223d5c5019de31c1
ccQpein <ccqpein@protonmail.com>
2020-03-16 17:43:55 -0400

    need input this thing

--- test.js
+++ test.js
@@ -1,9 +1,19 @@
 'use strict';
 
+//<script src="https://unpkg.com/react@16/umd/react.development.js" crossorigin></script>
+//<script src="https://unpkg.com/react-dom@16/umd/react-dom.development.js" crossorigin></script>
+//<script src="https://unpkg.com/babel-standalone@6/babel.min.js"></script>
+
 let t = document.createElement('div');
 t.setAttribute("id", "menu");
-let b = document.getElementsByTagName("body");
-b[0].insertAdjacentElement("afterbegin", t);
+t.setAttribute("display","inline-block");
+
+let article = document.getElementsByClassName("markdown-body")[0];
+article.setAttribute("display","inline-block");
+
+let b = document.getElementsByTagName("body")[0];
+//b.setAttribute("float","left");
+b.insertAdjacentElement("afterbegin", t);
 
 class LikeButton extends React.Component {
   constructor(props) {
@@ -20,7 +30,7 @@ class LikeButton extends React.Component {
       <button onClick={() => this.setState({ liked: true })}>
         Like
       </button>
-);
+    );
   }
 }
 
